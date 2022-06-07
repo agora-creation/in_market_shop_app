dd27bd045fb2013ef523ba12b8f7e3704a99406e
agora.creation <apple@agora-c.com>
2022-06-07 13:03:27 +0900

    20220607 commit

--- lib/screens/order_add.dart
+++ lib/screens/order_add.dart
@@ -1,22 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:in_market_shop_app/models/shop.dart';
+import 'package:in_market_shop_app/models/user.dart';
+import 'package:in_market_shop_app/providers/auth.dart';
+import 'package:in_market_shop_app/providers/user.dart';
+import 'package:in_market_shop_app/widgets/item_card.dart';
+import 'package:in_market_shop_app/widgets/not_list_message.dart';
+import 'package:provider/provider.dart';
 
-class OrderAddScreen extends StatelessWidget {
+class OrderAddScreen extends StatefulWidget {
   const OrderAddScreen({Key? key}) : super(key: key);
 
   @override
+  State<OrderAddScreen> createState() => _OrderAddScreenState();
+}
+
+class _OrderAddScreenState extends State<OrderAddScreen> {
+  @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    ShopModel? shop = authProvider.shop;
+    final userProvider = Provider.of<UserProvider>(context);
+    List<UserModel> users = [];
+
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blue.shade100,
       appBar: AppBar(
+        backgroundColor: Colors.blue.shade100,
         automaticallyImplyLeading: false,
         centerTitle: true,
-        title: const Text('注文する'),
+        title: const Text('注文する - 注文者選択'),
         actions: [
           IconButton(
             onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
             icon: const Icon(Icons.close),
           ),
         ],
+      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: userProvider.streamUsers(shop: shop),
+        builder: (context, snapshot) {
+          users.clear();
+          if (snapshot.hasData) {
+            for (DocumentSnapshot<Map<String, dynamic>> doc
+                in snapshot.data!.docs) {
+              users.add(UserModel.fromSnapshot(doc));
+            }
+          }
+          if (users.isEmpty) {
+            return const NotListMessage(message: '注文者がいません');
+          }
+          return GridView.builder(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(24),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 5,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+            ),
+            itemCount: users.length,
+            itemBuilder: (_, index) {
+              UserModel user = users[index];
+              return ItemCard(
+                name: user.name,
+                onTap: () {},
+              );
+            },
+          );
+        },
       ),
     );
   }
