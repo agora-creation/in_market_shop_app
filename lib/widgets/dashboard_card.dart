fb7d1bdd09d14d98d709f0522290cb437d4e94b4
agora.creation <apple@agora-c.com>
2022-06-09 19:02:15 +0900

    20220609 commit

--- lib/widgets/dashboard_card.dart
+++ lib/widgets/dashboard_card.dart
@@ -26,61 +26,64 @@ class DashboardCard extends StatelessWidget {
       mainAxisCellCount: mainAxisCellCount,
       child: GestureDetector(
         onTap: onTap,
-        child: Card(
-          color: Colors.white,
-          elevation: 3,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              count != 0
-                  ? Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Align(
-                        alignment: Alignment.bottomRight,
-                        child: Chip(
-                          backgroundColor: Colors.red.shade400,
-                          label: Text(
-                            '$count',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'SourceHanSans-Bold',
+        child: Container(
+          height: 100,
+          child: Card(
+            color: Colors.white,
+            elevation: 3,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                count != 0
+                    ? Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Chip(
+                            backgroundColor: Colors.red.shade400,
+                            label: Text(
+                              '$count',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'SourceHanSans-Bold',
+                              ),
                             ),
                           ),
                         ),
+                      )
+                    : Container(),
+                Expanded(
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Icon(
+                        iconData,
+                        color: Colors.blue,
                       ),
-                    )
-                  : Container(),
-              Expanded(
-                child: FittedBox(
-                  fit: BoxFit.fill,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Icon(
-                      iconData,
-                      color: Colors.blue,
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 32),
-                child: Text(
-                  labelText ?? '',
-                  style: const TextStyle(
-                    color: Colors.blue,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'SourceHanSans-Bold',
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child: Text(
+                    labelText ?? '',
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SourceHanSans-Bold',
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
