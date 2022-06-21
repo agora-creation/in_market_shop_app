ed2bc0380db31a376735f457b316002947174290
agora.creation <apple@agora-c.com>
2022-06-21 17:16:55 +0900

    20220621 commit

--- lib/widgets/image_card2.dart
+++ lib/widgets/image_card2.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:in_market_shop_app/helpers/style.dart';
 
 class ImageCard2 extends StatelessWidget {
   final String imageUrl;
@@ -19,70 +18,89 @@ class ImageCard2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.7),
-            spreadRadius: 0.1,
-            offset: const Offset(0.4, 0.5),
-          ),
-        ],
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.white,
-      ),
+    return Card(
+      elevation: 3,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          GestureDetector(
-            onTap: onTap,
-            child: SizedBox(
-              width: 300,
-              height: 200,
-              child: imageUrl != ''
-                  ? Image.network(
-                      imageUrl,
-                      fit: BoxFit.cover,
-                    )
-                  : Image.asset(
-                      noImagePath,
-                      fit: BoxFit.cover,
-                    ),
+          SizedBox(
+            height: 250,
+            child: Ink.image(
+              image: NetworkImage(imageUrl),
+              fit: BoxFit.cover,
             ),
           ),
-          Padding(
+          Container(
             padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'SourceHanSans-Bold',
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                subTitle != null ? const SizedBox(height: 4) : Container(),
-                subTitle != null
-                    ? Text(
-                        subTitle ?? '',
-                        style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'SourceHanSans-Bold',
-                        ),
-                      )
-                    : Container(),
-              ],
-            ),
+            alignment: Alignment.centerLeft,
+            child: Text(title),
           ),
-          child ?? Container(),
         ],
       ),
     );
+    // return Container(
+    //   decoration: BoxDecoration(
+    //     boxShadow: [
+    //       BoxShadow(
+    //         color: Colors.grey.withOpacity(0.7),
+    //         spreadRadius: 0.1,
+    //         offset: const Offset(0.4, 0.5),
+    //       ),
+    //     ],
+    //     borderRadius: BorderRadius.circular(4),
+    //     color: Colors.white,
+    //   ),
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       GestureDetector(
+    //         onTap: onTap,
+    //         child: SizedBox(
+    //           width: 300,
+    //           height: 200,
+    //           child: imageUrl != ''
+    //               ? Image.network(
+    //                   imageUrl,
+    //                   fit: BoxFit.cover,
+    //                 )
+    //               : Image.asset(
+    //                   noImagePath,
+    //                   fit: BoxFit.cover,
+    //                 ),
+    //         ),
+    //       ),
+    //       Padding(
+    //         padding: const EdgeInsets.all(16),
+    //         child: Column(
+    //           mainAxisAlignment: MainAxisAlignment.center,
+    //           crossAxisAlignment: CrossAxisAlignment.start,
+    //           children: [
+    //             Text(
+    //               title,
+    //               style: const TextStyle(
+    //                 fontSize: 16,
+    //                 fontWeight: FontWeight.bold,
+    //                 fontFamily: 'SourceHanSans-Bold',
+    //                 overflow: TextOverflow.ellipsis,
+    //               ),
+    //             ),
+    //             subTitle != null ? const SizedBox(height: 4) : Container(),
+    //             subTitle != null
+    //                 ? Text(
+    //                     subTitle ?? '',
+    //                     style: const TextStyle(
+    //                       color: Colors.grey,
+    //                       fontSize: 13,
+    //                       fontWeight: FontWeight.bold,
+    //                       fontFamily: 'SourceHanSans-Bold',
+    //                     ),
+    //                   )
+    //                 : Container(),
+    //           ],
+    //         ),
+    //       ),
+    //       child ?? Container(),
+    //     ],
+    //   ),
+    // );
   }
 }
